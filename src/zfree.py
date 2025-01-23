3443e28b05655e085c5be9198d435e0f22110eda
multiplealiases <conflictvegetable@protonmail.com>
2025-01-23 19:36:01 +0800

    new version

--- src/zfree.py
+++ src/zfree.py
@@ -10,7 +10,7 @@ import math
 
 # one-number pride versioning:
 # increment every time the author is proud of the release
-__version__ = "1"
+__version__ = "2"
 
 
 def check_open_read(f: str) -> str:
